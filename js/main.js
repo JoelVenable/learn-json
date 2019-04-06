bfadf00700f80b8bad75219fd38430f4e363f827
Joel Venable <joelmusicman@gmail.com>
2019-04-05 19:49:35 -0500

    Can write to the page with a Javascript Object

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
-var obj1 = {
+var obj = [{
   user: "John",
   age: 22,
   country: "United States"
-};
-var obj2 = {
+}, {
   user: "Will",
   age: 27,
   country: "United Kingdom"
-};
-var obj3 = {
+}, {
   user: "Abiel",
   age: 19,
   country: "Mexico"
-};
+}];
+
+
+for (let i = 0; i < 3; i++) {
+
+  document.write(`<p>${obj[i].user} is ${obj[i].age} years old and lives in ${obj[i].country}.</p>`);
+  
+}
