8eb52b492e76c867e8ef1cc9f5d10c794775054b
Joel Venable <joelmusicman@gmail.com>
2019-04-05 19:42:33 -0500

    Added objects to main.js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var obj1 = {
+  user: "John",
+  age: 22,
+  country: "United States"
+};
+var obj2 = {
+  user: "Will",
+  age: 27,
+  country: "United Kingdom"
+};
+var obj3 = {
+  user: "Abiel",
+  age: 19,
+  country: "Mexico"
+};
