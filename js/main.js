0eba386d85ec3fd3cee0692411b87c2af9e6df00
Joel Venable <joelmusicman@gmail.com>
2019-04-05 20:41:37 -0500

    Built demo JSON file

--- js/main.js
+++ js/main.js
@@ -18,3 +18,12 @@ for (let i = 0; i < 3; i++) {
   document.write(`<p>${obj[i].user} is ${obj[i].age} years old and lives in ${obj[i].country}.</p>`);
   
 }
+
+var meats = ["beef", "pork", "lamb"];
+var fruit = ["apple", "plum", "grape", "orange"];
+
+var obj5 = { arr1: meats, arr2: fruit};
+
+
+document.write(obj5.arr1[1] + " with " +obj5.arr2[1] + " for dinner.");
+
